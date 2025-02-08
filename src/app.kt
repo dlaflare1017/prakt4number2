9130f1461bd06bd35433238fe7edb542e282d311
vlad <mr.xeris@mail.ru>
2025-02-08 23:57:22 +0200

    kotlin

--- src/app.kt
+++ src/app.kt
@@ -1,9 +1,18 @@
+//fun main() {
+//
+//    val list1 = listOf(1, 2, 3, 4, 5)
+//    val list2 = listOf(3, 4, 5, 6, 7)
+//
+//    val intersection = list1.intersect(list2).sorted()
+//
+//    println("Общие числа в порядке возрастания: $intersection")
+//}
+
 fun main() {
+    val set1 = setOf(1, 2, 3, 4, 5, 6)
+    val set2 = setOf(4, 5, 6, 7, 8, 9)
 
-    val list1 = listOf(1, 2, 3, 4, 5)
-    val list2 = listOf(3, 4, 5, 6, 7)
+    val commonNumbers = set1.intersect(set2).sorted()
 
-    val intersection = list1.intersect(list2).sorted()
-    
-    println("Общие числа в порядке возрастания: $intersection")
+    println(commonNumbers)
 }
